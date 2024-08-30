7ce39444d93ab085fd8c46cb53bc32a4e558b6ea
Divya Sonar <divyasonar346@gmail.com>
2024-08-30 17:32:19 +0530

    conversions of datatypes

---
+++ basics_js/datatypeconversion.js
@@ -0,0 +1,20 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 - true , 0 -false
+// ""(emptystring) - false
+// "abc" - true
+
+let someNumber = 32
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
