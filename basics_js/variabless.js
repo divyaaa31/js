f2faa07b492d5b46165ffee58d1cedf54ac30191
divyaaa31 <divyasonar346@gmail.com>
2024-08-30 09:37:59 +0000

    working with variables

---
+++ basics_js/variabless.js
@@ -0,0 +1,10 @@
+const accountId = 122
+let accountEmail = "abcxyz@gmail.com"
+var accountPassword = "123344"
+accountCity = "goa"
+
+accountEmail = "hchch@gmail.com"
+accountPassword = "234232"
+accountCity = "abad"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
