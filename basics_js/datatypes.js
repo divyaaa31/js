f97d1e5b7c156b3c8170c76186242a189e44676f
divyaaa31 <divyasonar346@gmail.com>
2024-08-30 10:48:08 +0000

    basic datatypes

---
+++ basics_js/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+console.log(3 + 3);
+console.log("abc");
+
+let name = "abc"
+let age = 17
+let isLoggedIn = false
+let state;
+
+// String-""
+// int
+// BigInt
+// Boolean-t/f
+// null- standalone value
+// undefined
+// symbol-unique
+
+console.log(typeof state);
+
+
