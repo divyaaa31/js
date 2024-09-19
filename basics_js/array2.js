23686dda3c5511b43b6272d8008bb2565c40f521
Divya Sonar <divyasonar346@gmail.com>
2024-09-19 16:53:35 +0530

    working with arrays

---
+++ basics_js/array2.js
@@ -0,0 +1,31 @@
+const a = ["abc", "xyz", "pqr"]
+const b = ["one", "two", "three"]
+
+// a.push(b);
+
+// console.log(a);
+
+// a.concat(b)
+// console.log(a);
+
+// const x = a.concat(b)
+// console.log(x);
+
+// const x = b.concat = [...a, ...b]
+
+// // console.log(x);
+
+// const arr = [1,2,3,[4,5],[6,6, [7,8]]]
+// const arr2 = arr.flat(Infinity)
+// console.log(arr2);
+
+console.log(Array.isArray('abc'));
+console.log(Array.from('abc'));
+console.log(Array.from({name: "ram"}));
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
+
